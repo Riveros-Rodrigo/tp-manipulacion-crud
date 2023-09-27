0c92f258dd63f3c4fd75fb9cca0297308ae4ffb5
Rodrigo Riveros <riverosdev@gmail.com>
2023-09-27 12:54:20 -0300

    primer commit

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,130 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const moment = require('moment')
+
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+    add: function (req, res) {
+        res.render('moviesAdd')
+    },
+    create: function (req, res) {
+        const {title, rating, awards, release_date,length} = req.body //viene del form
+        db.Movie.create({
+            title: title.trim(),
+            rating,
+            awards,
+            release_date,
+            length
+        })
+            .then(movie =>{
+                console.log(movie);
+                return res.redirect('/movies')
+            }).catch(error => console.log(error))
+    },
+    edit: function(req, res) {
+        db.Movie.findByPk(req.params.id) //siempre va esto porque necesito un id
+            .then(movie =>{
+                return res.render('moviesEdit',{
+                    Movie : movie, 
+                    moment
+                })
+            }).catch(error => console.log(error))
+    },
+    update: function (req,res) {
+        const {title, rating, awards, release_date,length} = req.body //viene del form
+        db.Movie.update(
+            {
+                title: title.trim(),
+                rating,
+                awards,
+                release_date,
+                length
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        ).then(movie =>{ //el then dsp de usar el update te devuelve un array con el valor de 1 si hubo cambio o 0 si no hubo cambio
+            db.Movie.findByPk(req.params.id)
+                .then(movie =>{ //mando la peli que consegui (movie)
+                    return res.render('moviesDetail',{
+                        movie //mando la peli que consegui (movie)
+                    })
+                })
+        }).catch(error => console.log(error))
+    },
+    delete: function (req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then(movie => {
+            res.render('moviesDelete.ejs', {Movie:movie});
+        }).catch(error => console.log(error))
+    },
+    destroy: function (req, res) {
+        //como algunas peliculas estan vinculadas con el fk tengo que hacer destroy otra vez
+        db.ActorMovie.destroy({
+            where:{movie_id: req.params.id}
+        }).then(moviesDestroy =>{
+            console.log(moviesDestroy);
+            //Acá hago update porque tenia actores con vincula en la pelicula ya que era su peli favorita, por eso hago update asi lo edita y me deja borrar la peli
+            db.Actor.update(
+                {
+                    favorite_movie_id:null
+                },
+                {
+                    where:{
+                        favorite_movie_id: req.params.id
+                    }
+            }).then(response =>{
+                console.log(response);
+                db.Movie.destroy({
+                    where:{id: req.params.id}
+                }).then(response =>{
+                    console.log(response);
+                    res.redirect('/movies')
+                })
+            })
+        }).catch(error => console.log(error))
+    }
+
+}
+
+module.exports = moviesController;
---
+++ src/routes/moviesRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/moviesController');
+
+router.get('/movies', moviesController.list);
+router.get('/movies/new', moviesController.new);
+router.get('/movies/recommended', moviesController.recomended);
+router.get('/movies/detail/:id', moviesController.detail);
+
+
+//Rutas exigidas para la creación del CRUD
+router.get('/movies/add', moviesController.add); // para agregar va con add
+router.post('/movies/create', moviesController.create); //para crear va con post
+router.get('/movies/edit/:id', moviesController.edit); //ruta parametrisada
+router.put('/movies/update/:id', moviesController.update); //para editar va el put
+router.get('/movies/delete/:id', moviesController.delete);
+router.delete('/movies/delete/:id', moviesController.destroy); // para eliminar delete
+
+module.exports = router;
